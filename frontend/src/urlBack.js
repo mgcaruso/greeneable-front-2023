b8de00e4f4ca487e00aac614b4e2f2b76ff5970f
unknown <mguadalupecaruso@gmail.com>
2023-02-15 14:31:32 -0300

    first commit greeneable front 2023

---
+++ frontend/src/urlBack.js
@@ -0,0 +1,5 @@
+// let apiUrl = "https://greeeneable-back.herokuapp.com/"
+// let urlBack = "https://greeneable-back-2023.up.railway.app"
+let urlBack = "http://localhost:4000"
+
+export default urlBack
